907f88ed2ce79dead61b086504329588fe68a9c8
CaptainOmNoms <CaptainOmNoms@users.noreply.github.com>
2019-06-12 11:16:00 -0500

    Create Ingredient resources

---
+++ resources/ingredient.py
@@ -0,0 +1,9 @@
+from flask_restful import Resource, reqparse
+from flask_jwt_extended import jwt_required
+from models.ingredients import IngredientModel
+
+class Ingredient(Resource):
+    
+
+class IngredientList(Resource):
+    
