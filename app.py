7da75dd889c3e72b5ad42d75b057bb20e468af0a
CaptainOmNoms <CaptainOmNoms@users.noreply.github.com>
2019-06-17 13:49:10 -0500

    Update app.py
    Adding jwt callbacks and blacklist uses. Also adding in resources

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
 from db import db
-from resources.user import User, UserLogin, UserLogout
+from blacklist import BLACKLIST
+from resources.user import User, UserLogin, UserLogout, TokenRefresh
 from resources.ingredient import Ingredient, IngredientList
 from resources.recipe import Recipe, RecipeList
+from resources.recipe_ingredient import RecipeIngredient, RecipeIngredientList
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -13,26 +15,71 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 api.Api(app)
 
+app.config['JWT_SECRET_KEY'] = 'joe'
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+jwt = JWTManager(app)
+
+@jwt.user_claims_loader
+def add_claims_to_jwt(identiy):
+    if identity == 1:
+        return {'admin': True}
+    return {'admin': False}
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    return decrypted_token['jti'] in BLACKLIST
+
+@jwt.expired_token_loader
+def expired_token_callback():
+    return jsonify({
+        'message': 'The token has expired.',
+        'error': 'token_expired'
+    }), 401
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):  # we have to keep the argument here, since it's passed in by the caller internally
+    return jsonify({
+        'message': 'Signature verification failed.',
+        'error': 'invalid_token'
+    }), 401
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return jsonify({
+        "description": "Request does not contain an access token.",
+        'error': 'authorization_required'
+    }), 401
+
+
+@jwt.needs_fresh_token_loader
+def token_not_fresh_callback():
+    return jsonify({
+        "description": "The token is not fresh.",
+        'error': 'fresh_token_required'
+    }), 401
+
+@jwt.revoked_token_loader
+def revoked_token_callback():
+    return jsonify({
+        "description": "The token has been revoked.",
+        'error': 'token_revoked'
+    }), 401
+
 @app.before_first_request
 def create_tables():
     db.create_all()
-
-jwt = JWTManager(app)
-
-# TODO: Add claims loader
-# TODO: Add jwt token refreshing and reloading
-
-
-# TODO: uncomment endpoints as resources/models are created
-
-# api.add_resource('Recipe', '/recipe/<int:recipe_id>')
-# api.add_resource('RecipeList', '/recipes')
-# api.add_resource('Ingredient', '/ingredient/<int:ingredient_id>')
-# api.add_resource('IngredientList', '/ingredients/')
-# api.add_resource('User', '/user/<int:user_id>')
-# api.add_resource('UserLogin', '/login')
-# api.add_resource('UserLogout', '/logout')
-
+    
+    
+api.add_resource('Recipe', '/recipe/<int:recipe_id>')
+api.add_resource('RecipeList', '/recipes')
+api.add_resource('Ingredient', '/ingredient/<int:ingredient_id>')
+api.add_resource('IngredientList', '/ingredients/')
+api.add_resource('RecipeIngredient', '/recipe_ingredient')
+api.add_resource('RecipeIngredientList', '/recipe_ingredients')
+#api.add_resource('User', '/user/<int:user_id>')
+api.add_resource('UserLogin', '/login')
+api.add_resource('UserLogout', '/logout')
 
 if __name__ == '__main__':
     db.init_app(app)
