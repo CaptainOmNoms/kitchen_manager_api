0f2ba3414d85d76beb9c84a19a47284bc830b335
CaptainOmNoms <CaptainOmNoms@users.noreply.github.com>
2019-06-17 13:19:35 -0500

    Create recipe_ingredient.py

---
+++ models/recipe_ingredient.py
@@ -0,0 +1,34 @@
+from db import db
+
+class RecipeIngredientModel(db.Model):
+	__tablename__ = 'recipe_ingredients'
+	
+	recipe_id = db.Column(db.Integer, db.ForeignKey('recipe_id'))
+	ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredient_id'))
+	amount = db.Column(db.Float(precision=2)
+	recipe = db.relationship('RecipeModel')
+	Ingredient = db.relationship('IngredientModel')
+	
+	def __init__(self, amount, recipe_id, ingredient_id):
+        self.recipe_id = recipe_id
+        self.ingredient = ingredient_id
+		self.amount = amount
+		
+	def json(self):
+		return {'amount': self.amount, 'ingredient': self.Ingredient.name}
+		
+	@classmethod
+	def find_by_recipe(cls, recipe_id):
+		return cls.query.filter_by(recipe_id=recipe_id)
+	
+	@classmethod
+	def find_by_recipe_ingredient(cls,recipe_id, ingredient_id):
+		return cls.query.filter_by(recipe_id=recipe_id).filter(ingredient_id=ingredient_id)
+	
+	def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
